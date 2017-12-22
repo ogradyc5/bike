698abb13288acbd5b2028586ae6608bffeb47042
catherine <catherine.ogrady5@mail.dcu.ie>
2017-12-22 22:28:20 +0000

    User

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
 devise_for :users,:controllers => { :registrations => "user/registrations"}
 resources :welcome
-
 root to: 'welcome#index'
-
-
+devise_scope :user do
+    get "Sign up", to: "devise/registrations#new"
+    match '/sessions.user', to: 'devise/sessions#create' , via: :post
+end
  match '*path', via: :all, to: redirect('/404')
 end
