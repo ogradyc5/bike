d4732aa06aaf243a83786fb681fd144e4ffcfd37
catherine <catherine.ogrady5@mail.dcu.ie>
2017-12-23 16:11:15 +0000

    reconfig

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 devise_for :users, :controllers => { :registrations => "user/registrations"}
-
+resources :users
 resources :store_bicycles do  
     resources :bookings  
 end  
